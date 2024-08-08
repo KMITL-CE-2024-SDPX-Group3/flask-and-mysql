b2c8306641cc440bcfc9591841fba66f10b479bd
sn-nast <sunsun.natchanon@gmail.com>
2024-08-08 20:41:45 +0700

    create config class

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config:
+    MYSQL_HOST = os.getenv("DB_HOST")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB")
+    DEBUG = os.getenv("FLASK_DEBUG")
